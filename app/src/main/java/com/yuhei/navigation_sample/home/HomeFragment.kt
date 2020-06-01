9a6f0d539b0c2ab6d8147317e5414f8c9768ddbb
Yuhei Kawakami <yuhei8902@gmail.com>
2020-06-01 15:02:51 +0900

    Add home screen

---
+++ app/src/main/java/com/yuhei/navigation_sample/home/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.yuhei.navigation_sample.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+import com.yuhei.navigation_sample.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    private val homeViewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            text_home.text = it
+        })
+
+        btn_to_dashboard.setOnClickListener {
+            Snackbar.make(view, "Button Clicked", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
