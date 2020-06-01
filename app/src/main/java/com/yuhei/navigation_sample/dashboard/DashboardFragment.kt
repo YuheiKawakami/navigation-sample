a6b944afb516d6e3496ac522b77091abf8b3f0f2
Yuhei Kawakami <yuhei8902@gmail.com>
2020-06-01 15:26:42 +0900

    Add dashboard screen

---
+++ app/src/main/java/com/yuhei/navigation_sample/dashboard/DashboardFragment.kt
@@ -0,0 +1,32 @@
+package com.yuhei.navigation_sample.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.yuhei.navigation_sample.R
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+
+class DashboardFragment : Fragment() {
+
+    private val dashboardViewModel: DashboardViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            text_dashboard.text = it
+        })
+    }
+}
